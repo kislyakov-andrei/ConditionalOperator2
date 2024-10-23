2c07090dd079f50d06eb159631f594b6e04756c3
kislyakov-andrei <kisli@mail.ru>
2024-10-23 23:04:18 +0300

    add Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        // Task 1
+        System.out.println("Task 1");
+        int clientOS=1;
+       if (clientOS==0) {
+           System.out.println("Установите версию приложения для iOS по ссылке.");
+       }
+       else {
+           System.out.println("Установите версию Android по ссылке.");
+       }
+       // Task 2
+        System.out.println("Task 2");
+
+
     }
 }
