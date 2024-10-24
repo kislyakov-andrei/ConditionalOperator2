2d2b6066da6b9cc8089b650cdeba4e094f78ad59
kislyakov-andrei <kisli@mail.ru>
2024-10-25 00:06:30 +0300

    add Task 4

--- src/Main.java
+++ src/Main.java
@@ -17,12 +17,43 @@ public class Main {
                 System.out.println("Установите версию приложения для iOS по ссылке.");
             } else {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-            }}
-        else if (clientDeviceYear >= 2015) {
-                System.out.println("Установите  версию приложения для Android по ссылке.");
-            } else {
-                System.out.println("Установите облегченную версию для  Android по ссылке.");
             }
+        } else if (clientDeviceYear >= 2015) {
+            System.out.println("Установите  версию приложения для Android по ссылке.");
+        } else {
+            System.out.println("Установите облегченную версию для  Android по ссылке.");
+        }
+        // Task 3
+        System.out.println("Task 3");
+        int year = 2021;
 
-    }
+        if (year > 1584) {
+            if (year % 4 == 0 || (year % 400 == 0 && year % 100 != 0)) {
+                System.out.println(year + " год является високосным.");
+            } else {
+                System.out.println(year + " год не является високосным.");
+            }
+        } else {
+            System.out.println(year + " год не является високосным.");
+        }
+
+        // Task 4
+        System.out.println("Task 4");
+        int deliveryDistance = 99;
+        int deliveryTime = 1;
+        if (deliveryDistance <= 100) {
+            if (deliveryDistance <= 20) {
+                System.out.println("Потребуется дней" + deliveryTime);
+            } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+                System.out.println("Потребуется дней " + (deliveryTime += 1));
+            }
+         else {
+            System.out.println("Потребуется дней" + (deliveryTime += 2));
+        }}
+        else{
+            System.out.println("Доставки нет.");}
+
+
+
+}
 }
