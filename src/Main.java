d3d99f18daca796c61921122381de28bca5a4829
kislyakov-andrei <kisli@mail.ru>
2024-10-25 20:25:58 +0300

    homwork to check

--- src/Main.java
+++ src/Main.java
@@ -39,19 +39,46 @@ public class Main {
 
         // Task 4
         System.out.println("Task 4");
-        int deliveryDistance = 99;
+        int deliveryDistance = 95;
         int deliveryTime = 1;
         if (deliveryDistance <= 100) {
             if (deliveryDistance <= 20) {
-                System.out.println("Потребуется дней" + deliveryTime);
+                System.out.println("Потребуется дней: " + deliveryTime);
             } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-                System.out.println("Потребуется дней " + (deliveryTime += 1));
+                System.out.println("Потребуется дней: " + (deliveryTime += 1));
             }
          else {
-            System.out.println("Потребуется дней" + (deliveryTime += 2));
+            System.out.println("Потребуется дней: " + (deliveryTime += 2));
         }}
         else{
             System.out.println("Доставки нет.");}
+        // Task 5
+        System.out.println("Task 5");
+        int monthNumber=6;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println(monthNumber +" месяц года относится к сезону Зима.");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println(monthNumber+ " месяц года относится к сезону Весна.");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println(monthNumber+" месяц года относится к сезону Лето.");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println(monthNumber + " месяц года относится к сезону Осень.");
+                break;
+            default:
+                System.out.println("В году 12 месяцев. " + monthNumber + " месяца не существует.");
+        }
 
 
 
